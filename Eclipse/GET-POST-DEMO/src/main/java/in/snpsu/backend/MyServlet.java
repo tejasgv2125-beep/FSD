2cc6f6a9972cd1f4ab648512b6403d9be5f15325
tejasgv2125-beep <tejasgv2125@gmail.com>
2026-04-25 10:42:54 +0530

    commit

--- Eclipse/GET-POST-DEMO/src/main/java/in/snpsu/backend/MyServlet.java
+++ Eclipse/GET-POST-DEMO/src/main/java/in/snpsu/backend/MyServlet.java
@@ -7,13 +7,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-@WebServlet("/")
+@WebServlet("/loginForm")
 public class MyServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req,
 			HttpServletResponse resp) throws ServletException, IOException {
 		String email=req.getParameter("email");
-		String password=req.getParameter("Password");
+		String password=req.getParameter("password");
 		if(email.equals("ca@gmail.com")&& password.equals("admin123")) {
 			System.out.println("Success");
 		}else {
